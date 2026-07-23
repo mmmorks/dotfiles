e2129167cdaa6ccd4512671e02aadb83b1cd75c9
mmmorks <mmmorks@mmmorks.net>
2026-07-23 00:09:08 -0700

    fix: correct extras.lua comment on util.dot's actual scope

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -1,10 +1,11 @@
 return {
   { import = "lazyvim.plugins.extras.lang.python" },
   -- NOTE: `lazyvim.plugins.extras.lang.sh` does not exist in current LazyVim
-  -- (v16.0.0 / main as of 2026-07-22). Upstream folded bashls + shellcheck
-  -- into `util.dot` ("Language support for dotfiles") back in PR #2879;
-  -- there is no dedicated lang.sh module to import. Using the functional
-  -- equivalent so shell LSP/lint support is actually enabled.
+  -- (v16.0.0 / main as of 2026-07-22). Upstream folded it into `util.dot`
+  -- (Language support for dotfiles, PR #2879). Beyond bash/shell support,
+  -- util.dot unconditionally adds git_config treesitter parser and (since
+  -- ~/.config/fish exists) actively enables fish filetype/treesitter support;
+  -- hyprlang and rasi remain inert (no rofi/hyprland configs present).
   { import = "lazyvim.plugins.extras.util.dot" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
 }
